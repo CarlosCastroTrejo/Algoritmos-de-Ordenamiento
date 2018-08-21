692db72412420985eb8ecd012a7cca66907d70e0
Carlos Emiliano Castro Trejo <carlos_castro5156@outlook.es>
2018-08-20 22:24:36 -0500

    BubbleSort method is finished.

--- BubbleSort/BubbleSort/Program.cs
+++ BubbleSort/BubbleSort/Program.cs
@@ -6,6 +6,48 @@ namespace BubbleSort
     {
         public static void Main(string[] args)
         {
+            Random random = new Random();
+
+            int[] array = new int[20];
+
+            for (int x = 0; x < 20;x++)
+            {
+                int number = random.Next(0, 100);
+                array[x] = number;
+            }
+
+            bubbleSort(array);
+
+            for (int x = 0; x < array.Length; x++)
+            {
+                Console.WriteLine(array[x]);
+            }
+        }
+
+
+
+        public static void bubbleSort(int[] array)
+        {
+            bool sorted = false;
+            int number = array.Length - 1;
+            int temporal = 0;
+
+            while(!sorted)
+            {
+                sorted = true;
+                for (int x = 0; x < number;x++)
+                {
+                    if(array[x]>array[x+1])
+                    {
+                        sorted = false;
+                        temporal = array[x + 1];
+                        array[x + 1] = array[x];
+                        array[x] = temporal;
+                    }
+
+                }
+                number--;
+            }
         }
     }
 }
