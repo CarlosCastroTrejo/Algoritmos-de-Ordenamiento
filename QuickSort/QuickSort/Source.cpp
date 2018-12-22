55ff76ef2f210eb3cf6d6d8747b0796ba5b92ed2
Carlos Emiliano Castro Trejo <carlos_castro5156@outlook.es>
2018-12-22 13:24:28 -0600

    Quicksort algorithm was modified and it works perfectly with arrays.

--- QuickSort/QuickSort/Source.cpp
+++ QuickSort/QuickSort/Source.cpp
@@ -3,41 +3,42 @@
 
 using namespace std;
 
-static int dividir(int arreglo[],int inicio, int fin)
+
+static void quicksort(int arreglo[], int inicio, int fin)
 {
 	int izq, der, pivote, temp;
-	pivote = arreglo[inicio];
+	pivote = arreglo[(inicio+fin)/2];
 	izq = inicio;
 	der = fin;
-	while (izq < der)
+	while (izq <= der)
 	{
-		while (arreglo[der] > pivote)
+		while (arreglo[der] > pivote) 
+		{
 			der--;
-		while ((izq < der) &&
-			(arreglo[izq] <= pivote))
+		}
+		while (arreglo[izq] < pivote) 
+		{
 			izq++;
-		if (izq < der)
+		}
+		if (izq <= der)
 		{
 			temp = arreglo[izq];
 			arreglo[izq] = arreglo[der];
 			arreglo[der] = temp;
+			izq++;
+			der--;
 		}
 	}
-	temp = arreglo[der];
-	arreglo[der] = arreglo[inicio];
-	arreglo[inicio] = temp;
-	return der;
-}
-
-static void quicksort(int arreglo[], int inicio, int fin)
-{
-	int pivote;
-	if (inicio < fin)
+	
+	if (inicio < der) 
 	{
-		pivote = dividir(arreglo, inicio, fin);
-		quicksort(arreglo, inicio, pivote - 1);
-		quicksort(arreglo, pivote + 1, fin);
+		quicksort(arreglo, inicio, der);
 	}
+	if (izq < fin) 
+	{
+		quicksort(arreglo, izq, fin);
+	}
+	
 }
 
 int main() 
@@ -45,7 +46,8 @@ int main()
 
 	int arreglo[10]{ 231,564,2131,2312,2333,11,22,33,44,1 };
 
-	quicksort(arreglo,0,sizeof(arreglo)/sizeof(arreglo[0]));
+	
+	quicksort(arreglo,0,sizeof(arreglo)/sizeof(arreglo[0])-1);
 
 	for (int x = 0; x < 10; x++) 
 	{
